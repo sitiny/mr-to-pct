1e6301ae2be06ae2ac704e77da03acc8e30a60b8
sitiny <39376122+sitiny@users.noreply.github.com>
2023-04-17 17:59:45 +0800

    Updated infer_funcs.py

--- utils/infer_funcs.py
+++ utils/infer_funcs.py
@@ -2,10 +2,10 @@ import sys, os, copy
 import torch
 from monai.config import print_config
 from monai.transforms import (
-    AddChannel, Compose, LoadImage,
+    AddChannel, Compose, LoadImage, EnsureChannelFirst,
     ScaleIntensity, ToTensor, HistogramNormalize, ResizeWithPadOrCrop
 )
-from monai.data import write_nifti
+from monai.data import write_nifti, NibabelWriter
 from utils.netdef import ShuffleUNet
 import numpy as np
 import ants
@@ -29,19 +29,11 @@ def do_inference_3D(net, x, device, dims=(256,256), patch_size=(256,256,32), ste
 
     num_overlaps = np.zeros(y_pad.shape)
 
-    xptrans = Compose(
-        [
-            AddChannel(),
-            AddChannel(),
-            ToTensor(),
-        ]
-    )
-
     # iterate through patches & infer
     zz = 0
     while zz + z_size < x_pad.shape[2]:
-        xp = x_pad[:,:,zz:zz+z_size]
-        txp = xptrans(xp).to(device)
+        xp = x_pad[np.newaxis,np.newaxis,:,:,zz:zz+z_size]
+        txp = ToTensor()(xp).to(device)
         output = net(txp)
         output = output[0,0,...].cpu().data.numpy()
         y_pad[:,:,zz:zz+z_size] += output
@@ -62,7 +54,7 @@ def do_inference_3D(net, x, device, dims=(256,256), patch_size=(256,256,32), ste
 def do_prep_t1(img_file):
     img = ants.image_read(img_file)
     img_n4 = ants.n4_bias_field_correction(img)
-    img_tmp = img_n4.otsu_segmentation(k=1) # otsu_segmentation
+    img_tmp = img_n4.otsu_segmentation(k=3) # otsu_segmentation
     img_tmp = ants.multi_label_morphology(img_tmp, 'MD', 2) # dilate 2
     img_tmp = ants.smooth_image(img_tmp, 3) # smooth 3
     img_tmp = ants.threshold_image(img_tmp, 0.5) # threshold 0.5
@@ -87,7 +79,7 @@ def do_mr_to_pct(input_mr_file, output_pct_file, saved_model, device, prep_t1, p
         [
             HistogramNormalize(num_bins=256, min=0, max=255),
             ScaleIntensity(minv=-1.0, maxv=1.0),
-            AddChannel(),
+            EnsureChannelFirst(),
             ResizeWithPadOrCrop(spatial_size, mode='minimum'),
         ]
     )
@@ -112,8 +104,11 @@ def do_mr_to_pct(input_mr_file, output_pct_file, saved_model, device, prep_t1, p
     with torch.no_grad():
         ct_out = do_inference_3D(net, t1_arr, device)
 
-    ct_out = ResizeWithPadOrCrop(t1_meta['spatial_shape'], mode='minimum')(AddChannel()(ct_out))
-    write_nifti(ct_out[0], (output_pct_file), affine=t1_meta['affine'])
+    ct_out = ResizeWithPadOrCrop(t1_meta['spatial_shape'], mode='minimum')(ct_out[np.newaxis,:])
+    writer = NibabelWriter()
+    writer.set_data_array(ct_out[0,...], channel_dim=None)
+    writer.set_metadata({"affine": orig_t1_arr.affine})
+    writer.write(output_pct_file, verbose=True)
     print('MR to pCT done, output saved to: {}'.format(output_pct_file))
     print('')
     print('Please inspect your output.')
